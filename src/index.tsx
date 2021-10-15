4ccfc6e5c05f31cd5fb411fcfba9e08113e87710
VlasMaskalenchik <vlasmaskalenchik1998@gmail.com>
2021-10-15 22:47:27 +0300

    ad

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,10 @@ import {Header} from './Components/Header/Header';
 import {Provider} from 'react-redux'
 import {store} from './Redux/ReduxStore'
 import './index.css';
-
+console.log('2')
+console.log('2')
+console.log('2')
+console.log('2')
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
